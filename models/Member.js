e3bc7964db12b688ae09af92bf3ba9f511b9f0a5
Fauzi Afif Nevandi <40476170+fauziafifnevandi@users.noreply.github.com>
2021-11-29 05:54:24 +0700

    feature
    something new

--- models/Member.js
+++ models/Member.js
@@ -3,20 +3,20 @@ const mongoose = require("mongoose");
 const memberSchema = new mongoose.Schema({
   firstName: {
     type: String,
-    require: true,
+    required: true,
   },
   lastName: {
     type: String,
-    require: true,
+    required: true,
   },
   email: {
     type: String,
-    require: true,
+    required: true,
   },
   phoneNumber: {
     type: String,
-    require: true,
+    required: true,
   },
 });
 
-module.exports = mongoose.module("Member", memberSchema);
+module.exports = mongoose.model("Member", memberSchema);
